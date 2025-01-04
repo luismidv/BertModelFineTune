c9745661fbee2512b5bde69cbf22c76fc4c183a9
Luis Miguel Pérez <perezmartluismiguel@gmail.com>
2025-01-04 18:13:16 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from transformers import BertModel
+from torch import nn
+from torch import optim
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+
+#GET THE DATASETS
+dataset = pd.read_parquet('./data/train-00000-of-00001.parquet')
+print(dataset.head())
+#INITIALIZE TOKENIZER
+
+autotokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-cased")
+#INSTANTIATE THE MODEL
+model = BertModel.from_pretrained('bert-base-uncased')
+
+#CREATE CLASSIFIER
+classifier = nn.Linear(768, 2)
+
+#CONCATENATE
+model = nn.Sequential(model, classifier)
+
+#LOSS FN AND OPTIMIZER
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = 2e-5)
+
+frase = "hola"
+
+result = model(frase)
+print(result)
